a94842244db30e8c154f85bc7dc5c884f8592111
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2024-05-28 09:03:07 -0700

    Update to Slope intercept test

--- samples/types/Program.cs
+++ samples/types/Program.cs
@@ -8,8 +8,10 @@ var engine = new RecalcEngine(config);
 var text = @"
 Point = Type({ x: Number, y: Number });
 Line = Type({ start: Point, end: Point }); 
-LineEqual(a: Line, b: Line): Boolean = And(And(a.start.x=b.start.x,a.start.y=b.start.y),And(a.end.x=b.end.x,a.end.y=b.end.y));
-Intersects(a: Line, b: Line): Boolean = If(LineEqual(a,b), true, With( { slopeA: (a.end.y - a.start.y)/(a.end.x - a.start.x), slopeB: (b.end.y - b.start.y)/(b.end.x - b.start.x) },  slopeA <> slopeB) );";
+SlopeIntercept = Type({ slope: Number, intercept: Number });
+SlopeInterceptForm(l: Line): SlopeIntercept = With( { m: ((l.end.y - l.start.y)/(l.end.x - l.start.x)) }, { slope: m, intercept: (l.start.y - (m*l.start.x)) });
+LineEqual(a: Line, b: Line): Boolean = With( { line1: SlopeInterceptForm(a), line2: SlopeInterceptForm(b) }, And(line1.slope=line2.slope,line1.intercept=line2.intercept) );
+Intersects(a: Line, b: Line): Boolean = If(LineEqual(a,b), true, With( { line1: SlopeInterceptForm(a), line2: SlopeInterceptForm(b)}, line1.slope <> line2.slope) );";
 
 engine.AddUserDefinitions(text);
 
@@ -21,4 +23,3 @@ Console.WriteLine(result.Value);
 
 result = (BooleanValue)engine.Eval("With( { a: {start:{x:0.0, y:0.0}, end: {x:1.0, y:1.0}}, b: {start:{x:0.0, y:1.0}, end: {x:1.0, y:2.0}} }, Intersects(a,b))");
 Console.WriteLine(result.Value);
-
